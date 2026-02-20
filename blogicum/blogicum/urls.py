4656028936b77a9e0044fac234f27cadcd5a0c3d
tim8006 <150826881+tim8006@users.noreply.github.com>
2026-02-20 18:27:44 +0700

    Delete blogicum directory

--- blogicum/blogicum/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("", include("blog.urls")),
-    path("pages/", include("pages.urls")),
-    path("admin/", admin.site.urls),
-]
